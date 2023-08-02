23b6a1478e58133d01eb1ab01dc783680a02c2f3
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-08-02 11:48:44 -0400

    lab Complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,32 @@
 def badge_maker(name):
-    return None
+    badge_name="Hello, my name is " + name + "."
+    return badge_name
 
 def batch_badge_creator(names):
-    return None
+    badge_list=list()
+    for name in names:
+        badge_name="Hello, my name is " + name + "."
+        badge_list.append(badge_name)
+    return badge_list
 
 def assign_rooms(names):
-    return None
+    room_list=list();
+    room_number=1
+    for name in names:
+        msg="Hello, "+name+"! You'll be assigned to room "+str(room_number)+"!";
+        room_list.append(msg)
+        room_number=room_number+1;
+
+    return room_list
 
 def printer(names):
+    person_name=batch_badge_creator(names)
+   # print(person_name);
+    room_details= assign_rooms(names)
+  #  print(room_details)
+    for item in person_name:
+        print(item);
+    for lista in room_details:
+        print(lista);
+    
     return None
